c0754e2173ce6ad8bbad613579ca810f3c2b957c
cahrl <18103864@usc.edu.ph>
2024-11-01 01:12:15 +0800

    fixed memes

--- src/pages/BottomPage.jsx
+++ src/pages/BottomPage.jsx
@@ -1,13 +1,20 @@
+import meme1 from '../assets/meme1.png'
+import meme2 from '../assets/meme2.png'
+import meme3 from '../assets/meme3.png'
+import meme4 from '../assets/meme4.png'
+import meme5 from '../assets/meme5.png'
+import meme6 from '../assets/meme6.png'
+
 const BottomPage = () => (
     <div className="h-full flex flex-col items-center justify-center p-4 gap-8">
         <h1 className="text-2xl font-bold mb-4">MEMES WE MADE</h1>
         <div className="grid grid-cols-3 gap-4 w-full">
-            <img src="/src/assets/meme1.png" alt="Meme 1" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src="/src/assets/meme2.png" alt="Meme 2" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src="/src/assets/meme3.png" alt="Meme 3" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src="/src/assets/meme4.png" alt="Meme 4" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src="/src/assets/meme5.png" alt="Meme 5" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src="/src/assets/meme6.png" alt="Meme 6" className="border-2 border-customGreen rounded-lg object-cover" />
+            <img src={meme1} alt="Meme 1" className="border-2 border-customGreen rounded-lg object-cover" />
+            <img src={meme2} alt="Meme 2" className="border-2 border-customGreen rounded-lg object-cover" />
+            <img src={meme3} alt="Meme 3" className="border-2 border-customGreen rounded-lg object-cover" />
+            <img src={meme4} alt="Meme 4" className="border-2 border-customGreen rounded-lg object-cover" />
+            <img src={meme5} alt="Meme 5" className="border-2 border-customGreen rounded-lg object-cover" />
+            <img src={meme6} alt="Meme 6" className="border-2 border-customGreen rounded-lg object-cover" />
         </div>
     </div>
 );
