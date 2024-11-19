5f2e3b14bab2977874c3c0be71bb46c415125cdb
cahrlloyloy <18103864@usc.edu.ph>
2024-11-19 23:16:36 +0800

    fixed website

--- src/pages/BackPage.jsx
+++ src/pages/BackPage.jsx
@@ -1,22 +1,12 @@
 import { FaTelegramPlane } from "react-icons/fa";
-import { FaXTwitter, FaDiscord } from "react-icons/fa6";
+
 
 const BackPage = () => (
     <div className="flex flex-col items-center justify-center h-full gap-4">
-        <h1 className="text-3xl font-bold mb-4">SOCIALS</h1>
-        <div className="flex items-center gap-3">
-            <a href="https://x.com/shitlord40" target="_blank" rel="noopener noreferrer">
-                <button className="buttoncolors btn btn-outline text-customGreen hover:bg-customGreen hover:text-customBlue btn-circle lg:btn-lg md:btn-md sm:btn-sm">
-                    <FaXTwitter className="text-2xl" />
-                </button>
-            </a>
-            <a href="https://discord.gg/xF3EApfe" target="_blank" rel="noopener noreferrer">
-                <button className="buttoncolors btn btn-outline text-customGreen hover:bg-customGreen hover:text-customBlue btn-circle lg:btn-lg md:btn-md sm:btn-sm">
-                    <FaDiscord className="text-2xl" />
-                </button>
-            </a>
-            <a href="https://t.me/theshitlords" target="_blank" rel="noopener noreferrer">
-                <button className="buttoncolors btn btn-outline text-customGreen hover:bg-customGreen hover:text-customBlue btn-circle lg:btn-lg md:btn-md sm:btn-sm">
+        <h1 className="text-3xl font-bold mb-4">CONTACT US</h1>
+        <div className="flex w-3/6">
+            <a href="https://t.me/shitlordsmedia" target="_blank" rel="noopener noreferrer" className="w-full">
+                <button className="buttoncolors btn btn-outline text-customGreen btn-block hover:bg-customGreen hover:text-customBlue lg:btn-lg md:btn-md sm:btn-sm">
                     <FaTelegramPlane className="text-2xl" />
                 </button>
             </a>
