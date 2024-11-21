d687bfaa01f22e83d02de31638f94cff48a71d19
cahrlloyloy <18103864@usc.edu.ph>
2024-11-21 22:22:35 +0800

    final updates maybe

--- src/pages/BottomPage.jsx
+++ src/pages/BottomPage.jsx
@@ -9,12 +9,12 @@ const BottomPage = () => (
     <div className="h-full flex flex-col items-center justify-center p-4 gap-8">
         <h1 className="text-2xl font-bold mb-4">MEMES WE MADE</h1>
         <div className="grid grid-cols-3 gap-4 w-full">
-            <img src={meme1} alt="Meme 1" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src={meme2} alt="Meme 2" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src={meme3} alt="Meme 3" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src={meme4} alt="Meme 4" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src={meme5} alt="Meme 5" className="border-2 border-customGreen rounded-lg object-cover" />
-            <img src={meme6} alt="Meme 6" className="border-2 border-customGreen rounded-lg object-cover" />
+            <img src={meme1} alt="Meme 1" className="border-2 border-customGreen rounded-lg object-cover h-full" />
+            <img src={meme2} alt="Meme 2" className="border-2 border-customGreen rounded-lg object-cover h-full" />
+            <img src={meme3} alt="Meme 3" className="border-2 border-customGreen rounded-lg object-cover h-full" />
+            <img src={meme4} alt="Meme 4" className="border-2 border-customGreen rounded-lg object-cover h-full" />
+            <img src={meme5} alt="Meme 5" className="border-2 border-customGreen rounded-lg object-cover h-full" />
+            <img src={meme6} alt="Meme 6" className="border-2 border-customGreen rounded-lg object-cover h-full" />
         </div>
     </div>
 );
